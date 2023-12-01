0a66c21c83c58748fac2fd36b432d39000148939
Nikhil Kushwaha <osndl.nikhilk@aicofindia.com>
2023-12-01 17:29:53 +0530

    added grid

--- src/components/Pages/Login/Login.js
+++ src/components/Pages/Login/Login.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import classes from './Login.module.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className={classes.login_container}>
+      <div className={classes.login_grid}>
+        <div className={classes.grid_item}>1</div>
+        <div className={classes.grid_item}>2</div>
+        {/* <div className={classes.grid_item}>3</div>
+        <div className={classes.grid_item}>4</div>
+        <div className={classes.grid_item}>5</div>
+        <div className={classes.grid_item}>6</div>
+        <div className={classes.grid_item}>7</div>
+        <div className={classes.grid_item}>8</div>
+        <div className={classes.grid_item}>9</div> */}
+      </div>
+    </div>
   )
 }
 
