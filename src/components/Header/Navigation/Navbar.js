f76af323f825bd6eecfb598fb47107dfc3940c42
Nikhil Kushwaha <osndl.nikhilk@aicofindia.com>
2024-01-30 17:12:00 +0530

    modified navbar

--- src/components/Header/Navigation/Navbar.js
+++ src/components/Header/Navigation/Navbar.js
@@ -3,6 +3,10 @@ import { Link } from 'react-router-dom';
 import classes from './Navbar.module.css';
 import { signOut } from "firebase/auth";
 import {auth} from "../../../firebase"
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+import { styled, alpha } from '@mui/material/styles';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 
 const Navbar = () => {
   const userSignOut=()=>{
@@ -10,14 +14,78 @@ const Navbar = () => {
        console.log('sign out successful.'); 
     }).catch(error=>console.log(error))
 }
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  width: '100%',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
   return (
     <header>
-        <Link to="/"> <div className={classes.logo}> X </div> </Link>
+        <Link to="/"> <div className={classes.logo}><img className={classes.icon} src = 'Icons/Colored_LOGO.png' alt='twitter icon'></img></div> </Link>
+
+        <div className={classes.search_container}>
+              <input type="text" />
+              <button className={classes.search_btn}><SearchIcon/></button>
+            </div> 
         <nav>
-         <ul>
-            <li><Link to="/dashboard">Dash board</Link></li>
-            <li><button onClick={userSignOut}>Signout</button></li>
-         </ul>
+          <div className={classes.bookmark_container}>
+            <button className={classes.bookmark_btn}><BookmarkBorderIcon style={{ color: 'orange',fontSize: '2rem' }}/></button>
+            <button className={classes.signout_btn} onClick={userSignOut}>Signout</button>
+          </div>
+         {/* <ul>
+            <li>    */}
+      
+            {/* <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ 'aria-label': 'search' }}
+              />
+          </Search> */}
+
+          {/* </li>
+            <li><button className={classes.bookmark_btn}><BookmarkBorderIcon style={{ color: 'orange',fontSize: '2rem' }}/></button></li>
+            <li><button className={classes.signout_btn} onClick={userSignOut}>Signout</button></li>
+         </ul> */}
         </nav>
     </header>
   )
